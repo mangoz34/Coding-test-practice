f68a130ac5dabe7b75d1380d2ec6a3c5c2694980
Jerry Chung <79149095+mangoz34@users.noreply.github.com>
2025-11-16 16:14:24 -0800

    Create elevator.cpp

---
+++ elevator.cpp
@@ -0,0 +1,19 @@
+/*
+Core Function: External request, Internal Request, queue management, dispatching, movement controll
+
+Entity: Elevator, Elevator controll system, Dispatch Strategy, Request
+
+Properties:
+*/
+
+class ElevatorCar
+
+class ElevatorSystem
+
+class Request {
+  private:
+    int target_floor;
+    int pickup_floor;
+    
+
+class IDispatchStrategy
